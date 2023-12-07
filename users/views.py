38888e7d18c18036e57c5f87ec4cc441ed4c00ec
SpacecapsAlex <spacecapsalex@hotmail.com>
2023-12-07 20:17:59 +0300

    Added user_list page

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+from .models import User
 
 # Create your views here.
+
+
+def list_users(request: HttpRequest) -> HttpResponse:
+    return render(request, 'users/list_users.html', {'users': User.objects.all()})
