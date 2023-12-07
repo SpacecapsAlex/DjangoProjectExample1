4fdd39e127780e853d170077b56a03c831154e5c
SpacecapsAlex <spacecapsalex@hotmail.com>
2023-12-07 19:44:59 +0300

    Added home page

--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
 
 # Create your views here.
+
+
+def home(request: HttpRequest) -> HttpResponse:
+    return render(request, 'main/home.html', {'title': 'Home'})
